c04e1b14802487f2276938222083a3bf612c18c4
Nandini Bhardwaj <pnnandu808@gmail.com>
2026-05-29 20:13:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (19.74%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,48 +1,36 @@
 class Solution {
-    vector<bool> visit;
-    vector<vector<int>> adj;
-    unordered_set<int> cycle;
-    int cycleStart;
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        adj.resize(n + 1);
+        vector<int> indegree(n + 1, 0);
+        vector<vector<int>> adj(n + 1);
         for (auto& edge : edges) {
             int u = edge[0], v = edge[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
+            indegree[u]++;
+            indegree[v]++;
         }
 
-        visit.resize(n + 1, false);
-        cycleStart = -1;
-        dfs(1, -1);
+        queue<int> q;
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == 1) q.push(i);
+        }
+
+        while (!q.empty()) {
+            int node = q.front(); q.pop();
+            indegree[node]--;
+            for (int nei : adj[node]) {
+                indegree[nei]--;
+                if (indegree[nei] == 1) q.push(nei);
+            }
+        }
 
         for (int i = edges.size() - 1; i >= 0; i--) {
             int u = edges[i][0], v = edges[i][1];
-            if (cycle.count(u) && cycle.count(v)) {
+            if (indegree[u] == 2 && indegree[v])
                 return {u, v};
-            }
         }
         return {};
-    }
-
-private:
-    bool dfs(int node, int par) {
-        if (visit[node]) {
-            cycleStart = node;
-            return true;
-        }
-        visit[node] = true;
-        for (int nei : adj[node]) {
-            if (nei == par) continue;
-            if (dfs(nei, node)) {
-                if (cycleStart != -1) cycle.insert(node);
-                if (node == cycleStart) {
-                    cycleStart = -1;
-                }
-                return true;
-            }
-        }
-        return false;
     }
 };
