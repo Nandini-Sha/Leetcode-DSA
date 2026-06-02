ca3938bc34aae0f910b00b5e200b571edb480189
Nandini Bhardwaj <pnnandu808@gmail.com>
2026-06-02 06:53:28 +0530

    Time: 35 ms (95.33%) | Memory: 19.8 MB (81.12%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,51 +1,49 @@
 class Solution {
 public:
-    bool oneDiff(string a, string b) {
-        int diff = 0;
-
-        for (int i = 0; i < a.size(); i++) {
-            if (a[i] != b[i]) {
-                diff++;
-                if (diff > 1) return false;
-            }
-        }
-
-        return diff == 1;
-    }
-
-    int ladderLength(string beginWord,
-                     string endWord,
+    int ladderLength(string beginWord, string endWord,
                      vector<string>& wordList) {
 
-        unordered_set<string> words(
-            wordList.begin(),
-            wordList.end()
-        );
+        unordered_set<string> words(wordList.begin(), wordList.end());
 
         if (!words.count(endWord))
             return 0;
 
-        queue<pair<string,int>> q;
-        q.push({beginWord,1});
+        queue<string> q;
+        q.push(beginWord);
 
-        unordered_set<string> visited;
-        visited.insert(beginWord);
+        int level = 1;
 
-        while(!q.empty()) {
-            auto [word,len] = q.front();
-            q.pop();
+        while (!q.empty()) {
+            int sz = q.size();
 
-            if(word == endWord)
-                return len;
+            while (sz--) {
+                string cur = q.front();
+                q.pop();
 
-            for(auto &next : wordList) {
-                if(!visited.count(next) &&
-                   oneDiff(word,next)) {
+                if (cur == endWord)
+                    return level;
 
-                    visited.insert(next);
-                    q.push({next,len+1});
+                string temp = cur;
+
+                for (int i = 0; i < temp.size(); i++) {
+                    char original = temp[i];
+
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == original) continue;
+
+                        temp[i] = c;
+
+                        if (words.count(temp)) {
+                            q.push(temp);
+                            words.erase(temp);
+                        }
+                    }
+
+                    temp[i] = original;
                 }
             }
+
+            level++;
         }
 
         return 0;
