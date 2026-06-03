044b8dde5d0c12e7672b7bed9303c17e1de2d9d3
Nandini Bhardwaj <pnnandu808@gmail.com>
2026-06-03 07:44:27 +0530

    Time: 27 ms (98.17%) | Memory: 19.3 MB (95.62%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,51 +1,46 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord,
-                     vector<string>& wordList) {
-
-        unordered_set<string> words(wordList.begin(), wordList.end());
-
-        if (!words.count(endWord))
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if (find(wordList.begin(), wordList.end(), endWord) == wordList.end() ||
+            beginWord == endWord)
             return 0;
+        int m = wordList[0].size();
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        queue<string> qb, qe;
+        unordered_map<string, int> fromBegin, fromEnd;
+        qb.push(beginWord);
+        qe.push(endWord);
+        fromBegin[beginWord] = 1;
+        fromEnd[endWord] = 1;
 
-        queue<string> q;
-        q.push(beginWord);
-
-        int level = 1;
-
-        while (!q.empty()) {
-            int sz = q.size();
-
-            while (sz--) {
-                string cur = q.front();
-                q.pop();
-
-                if (cur == endWord)
-                    return level;
-
-                string temp = cur;
-
-                for (int i = 0; i < temp.size(); i++) {
-                    char original = temp[i];
-
+        while (!qb.empty() && !qe.empty()) {
+            if (qb.size() > qe.size()) {
+                swap(qb, qe);
+                swap(fromBegin, fromEnd);
+            }
+            int size = qb.size();
+            for (int k = 0; k < size; k++) {
+                string word = qb.front();
+                qb.pop();
+                int steps = fromBegin[word];
+                for (int i = 0; i < m; i++) {
                     for (char c = 'a'; c <= 'z'; c++) {
-                        if (c == original) continue;
-
-                        temp[i] = c;
-
-                        if (words.count(temp)) {
-                            q.push(temp);
-                            words.erase(temp);
+                        if (c == word[i])
+                            continue;
+                        string nei = word.substr(0, i) +
+                                     c + word.substr(i + 1);
+                        if (!wordSet.count(nei))
+                            continue;
+                        if (fromEnd.count(nei))
+                            return steps + fromEnd[nei];
+                        if (!fromBegin.count(nei)) {
+                            fromBegin[nei] = steps + 1;
+                            qb.push(nei);
                         }
                     }
-
-                    temp[i] = original;
                 }
             }
-
-            level++;
         }
-
         return 0;
     }
 };
