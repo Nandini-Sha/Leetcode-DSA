b20d6511e964883fd6847e8afd4e58fd1dd07c92
Nandini Bhardwaj <pnnandu808@gmail.com>
2026-07-09 10:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 112.6 MB (9.15%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        vector<int> allover;
-        int n = gas.size();
-        if(n==1 && gas[0]>=cost[0]) return 0;
-        int x;
-        int sum = 0;
-        for(int i = 0;i<n;i++){
-            x = gas[i]-cost[i];
-            sum+=x;
-            allover.push_back(x);
-        }
-        if(accumulate(allover.begin(), allover.end(), 0) < 0) return -1;
-        for(int i = 0;i<n;i++){
-            sum = 0;
-            if(allover[i]>0){
-                sum+=allover[i];
-                int j = (i+1)%n;
-                bool exist = true;
-                while(j!=i){
-                    sum+=allover[j];
-                    if(sum<0) {
-                        exist = false;
-                        break;
-                    }
-                    j = (j+1)%n;
-                }
-                if(exist) return i;
+
+        int total = 0;
+        int curr = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+
+            int diff = gas[i] - cost[i];
+
+            total += diff;
+            curr += diff;
+
+            if (curr < 0) {
+                start = i + 1;
+                curr = 0;
             }
         }
 
-        return -1;
+        return (total >= 0) ? start : -1;
     }
 };
