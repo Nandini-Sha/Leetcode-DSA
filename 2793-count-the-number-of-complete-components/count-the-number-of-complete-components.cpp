c0c23929755dd06081924956825c614b21b21629
Nandini Bhardwaj <pnnandu808@gmail.com>
2026-07-11 21:50:50 +0530

    Time: 33 ms (81.26%) | Memory: 128.3 MB (75.11%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    unordered_map <int,unordered_set<int>> mp;
-    unordered_map<int,bool> visited;
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        int m = edges.size();
-        for(int i = 0;i<n;i++){
-            mp[i].insert(i);
-            visited[i] = false;
-        }
-        for(int i = 0;i<m;i++){
-            mp[edges[i][0]].insert(edges[i][1]);
-            mp[edges[i][1]].insert(edges[i][0]);
-        }
-        int ans = 0;
-        for(int i = 0;i<n;i++){
-            if(visited[i]==false){
-                ans+=dfs(i,mp[i]);
+    vector<vector<int>> adj;
+    vector<bool> vis;
+
+    void dfs(int u, int &nodes, int &degSum){
+        vis[u] = true;
+        nodes++;
+        degSum+=adj[u].size();
+
+        for(int v:adj[u]){
+            if(!vis[v]){
+                dfs(v, nodes, degSum);
             }
         }
-
-        return ans;
     }
 
-    int dfs(int i, unordered_set<int>& s){
-        if(visited[i]) return 1;
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        adj.resize(n);
+        vis.assign(n,false);
 
-        visited[i] = true;
-
-        int ok = (s == mp[i]);   // current node valid?
-
-        for(auto j : mp[i]){
-            if(!visited[j]){
-                if(dfs(j, s) == 0)
-                    ok = 0;
-            }
+        for(auto &e: edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
 
-        return ok;
+        int ans = 0;
+
+        for(int i = 0;i<n;i++){
+            if(!vis[i]){
+                int nodes = 0;
+                int degSum = 0;
+
+                dfs(i, nodes, degSum);
+
+                if(degSum == nodes*(nodes-1)) ans++;
+            }
+        }
+        return ans;
     }
 };
