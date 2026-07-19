fa2da0aa56e360946f8170793387b8453684eab0
Nandini Bhardwaj <pnnandu808@gmail.com>
2026-07-19 18:25:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (79.99%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int> vis(26), num(26);
+        for(auto c:s) num[c-'a']++;
+        string stk;
+        for(auto c:s){
+            if(!vis[c-'a']){
+                while(!stk.empty() && stk.back()>c){
+                    if(num[stk.back()-'a']>0){
+                        vis[stk.back()-'a'] = 0;
+                        stk.pop_back();
+                    }
+                    else break;
+                }
+                vis[c-'a'] = 1;
+                stk.push_back(c);
+            }
+            num[c-'a'] -=1;
+        }
+
+        return stk;
+    }
+    
+};
