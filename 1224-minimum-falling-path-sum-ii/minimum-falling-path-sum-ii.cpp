5ecb6844a6208814f2e4eae0fd6847b0c2598f0c
Nandini Bhardwaj <pnnandu808@gmail.com>
2026-07-24 18:10:57 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (72.48%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<int> prev = grid[0];
-
-        for (int i = 1; i < n; i++) {
-            int min1 = INT_MAX, min2 = INT_MAX;
-            int col1 = -1, col2 = -1;
-
-            // Find smallest and second smallest values in previous DP row
-            for (int j = 0; j < n; j++) {
-                if (prev[j] < min1) {
-                    min2 = min1;
-                    col2 = col1;
-                    min1 = prev[j];
-                    col1 = j;
-                } else if (prev[j] < min2) {
-                    min2 = prev[j];
-                    col2 = j;
-                }
-            }
-
-            vector<int> curr(n);
-
-            // Compute current DP row
-            for (int j = 0; j < n; j++) {
-                if (j == col1)
-                    curr[j] = grid[i][j] + min2;
+        int n=grid.size();
+        
+        for(int i=1; i<n; i++){
+            auto row=grid[i-1];
+            nth_element(row.begin(), row.begin()+1, row.end());
+            for(int j=0; j<n; j++){
+                if (grid[i-1][j]==row[0])
+                    grid[i][j]+=row[1];
                 else
-                    curr[j] = grid[i][j] + min1;
+                    grid[i][j]+=row[0];
             }
-
-            prev = curr;
         }
-
-        return *min_element(prev.begin(), prev.end());
+        
+        return *min_element(grid[n-1].begin(), grid[n-1].end());
     }
 };
