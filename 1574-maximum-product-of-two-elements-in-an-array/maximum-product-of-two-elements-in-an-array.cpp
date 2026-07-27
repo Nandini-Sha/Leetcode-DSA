332e622f18a23452d826dc2ab68e545694b9038b
Nandini Bhardwaj <pnnandu808@gmail.com>
2026-07-27 08:20:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (99.05%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -14,6 +14,7 @@ public:
             if(nums[i]>=max){
                 prev = max;
                 max = nums[i];
+                
             }
             else if(nums[i]>prev){
                 prev = nums[i];
