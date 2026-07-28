e0c2dc619316b890caf3ac91b86136f04f2e44c5
Nandini Bhardwaj <pnnandu808@gmail.com>
2026-07-28 15:27:58 +0530

    Time: 23 ms (82.86%) | Memory: 54.4 MB (85.71%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        vector<int> a(26,0);
-        for(int i = 0;i<s.size();i++){
-            a[s[i]-'a'] ++;
+        int n = s.length();
+        int bucket[26] = {0};
+
+        for (int i = 0; i < n / 2; i++) {
+            bucket[s[i] - 'a']++;
         }
-        int j = 0;
-        int n = s.size();
-        s = "";
-        for(int i = 0;i<26;i++){
-            while(a[i]>=2){
-                char c = i+'a';
-                s.insert(j,2,c);
-                j++;
-                a[i]-=2;
+
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = 0; i < 26; i++) {
+            while (bucket[i] > 0) {
+                char c = i + 'a';
+                s[left++] = c;
+                s[right--] = c;
+                bucket[i]--;
             }
         }
-        for(int i = 0;i<26;i++){
-            if(a[i]==1){
-                char c = i+'a';
-                s.insert(j,1,c);
-                j++;
-                a[i]-=1;
-            }
-        }
+
         return s;
     }
 };
