2c5638372e80e6434d2161811ddb8ccf4183b529
Nandini Bhardwaj <pnnandu808@gmail.com>
2026-07-30 18:33:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (54.67%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,41 +2,10 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int n = word.size();
-        int ans = 0;
-        if(n<9){
-            ans += n;
-            return ans;
-        }
-        else{
-            ans+=8;
-            n-=8;
-        }
-        if(n<9){
-            ans+=(2*n);
-            return ans;
-        }
-        else{
-            ans+=16;
-            n-=8;
-        }
-        if(n<9){
-            ans = ans + (3*n);
-            return ans;
-        }
-        else{
-            
-            ans+=24;
-            n-=8;
-        }
-        if(n<9){
-            ans+=(4*n);
-            return ans;
-        }
-        else{
-            ans+=32;
-            n-=8;
-        }
-
-        return ans;
+        if(n>0 && n<9) return n;
+        if(n>8 && n<17) return 8+(2*(n-8));
+        if(n>16 && n<25) return 24+(3*(n-16));
+        if(n>24) return 48+(4*(n-24));
+        return -1;
     }
 };
